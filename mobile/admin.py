11f79e2591b4dfb08081f13336c02ee187be8927
Ahasan Habib Sany <46066881+Sany07@users.noreply.github.com>
2021-12-25 17:51:51 +0600

    mobile app & models created

---
+++ mobile/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Brand_Name)
+admin.site.register(Model_Name)
+admin.site.register(Color)
+
+class MobileAdmin(admin.ModelAdmin):
+    list_display = ('jan_code','brand_name','model_name','color',)
+admin.site.register(Mobile,MobileAdmin)
---
+++ mobile/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+class Brand_Name(models.Model):
+    brand_name = models.CharField(max_length=300)
+
+    class Meta:
+        verbose_name = "Brand Name"
+        verbose_name_plural = "Brand Names"
+
+    def __str__(self):
+        return self.brand_name
+
+
+class Model_Name(models.Model):
+    model_name = models.CharField(max_length=300,unique = True)
+
+    class Meta:
+        verbose_name = "Model Name"
+        verbose_name_plural = "Model Names"
+
+    def __str__(self):
+        return self.model_name     
+
+class Color(models.Model):
+    color = models.CharField(max_length=300)
+
+    class Meta:
+        verbose_name = "Color"
+        verbose_name_plural = "Colors"
+
+    def __str__(self):
+        return self.color   
+
+class Mobile(models.Model):
+    jan_code =  models.CharField(max_length = 300,unique = True)
+    brand_name = models.ForeignKey(Brand_Name, related_name='brand', on_delete=models.CASCADE) 
+    model_name = models.ForeignKey(Model_Name, related_name='model', on_delete=models.CASCADE) 
+    color = models.ForeignKey(Color,related_name='mobile_color',on_delete=models.CASCADE)
+    image = models.URLField(max_length=200,blank=True)
+
+
+    class Meta:
+        verbose_name = "Mobile Phone"
+        verbose_name_plural = "Mobile Phones"
+
+    def __str__(self):
+        return self.jan_code
